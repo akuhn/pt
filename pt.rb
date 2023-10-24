55f6b5c51452f8d42969077466b970c7fafbd35c
Adrian Kuhn <akuhn@iam.unibe.ch>
2023-10-24 18:52:10 +0100

    Use adaptive learning, ie boots fails and repeat wins after a while.

--- pt.rb
+++ pt.rb
@@ -8,6 +8,7 @@ Options = OptionsByExample.new("Usage: $0 [-i, --interactive]").parse(ARGV)
 DATABASE = SQLite3::Database.new('portugese_words_100.sqlite')
 fname = 'portugese_words_100.md'
 
+
 # Read the vocabulary file to extract word pairs, organizing them with unique
 # references for easy retrieval in the performance database.
 
@@ -30,6 +31,7 @@ File.foreach(fname) do |line|
   end
 end
 
+
 # Keep a database with quiz results for tracking progress over time, enabling
 # adaptive learning experiences that optimize repetitions and focus on areas of
 # improvement, enabling more effective and personalized learning journeys.
@@ -46,32 +48,56 @@ DATABASE.execute %{
   )
 }
 
-class Result < Struct.new(:key, :pt, :en, :dir, :success, :answer, :ts)
-
-  def fail?
-    success == 0
-  end
-
-  def win?
-    success == 1
-  end
-
-  def ask
-    dir.to_sym
-  end
+class Result < Struct.new(:key, :pt, :en, :ask, :success, :answer, :ts)
 end
 
-results = DATABASE.execute('SELECT * FROM `quiz_v2`').map { |each| Result.new *each }.group_by(&:key)
+results = DATABASE.execute('SELECT * FROM `quiz_v2`')
+  .map { |key, pt, en, ask, success, answer, ts|
+    Result.new key, pt, en, ask.to_sym, success == 1, answer, ts
+  }
+  .sort_by(&:ts).reverse
+
+successes = []
+probabilities = results
+  .group_by { |each| [each.key, each.ask] }
+  .map { |k, group|
+    if group.empty?
+      [k, 1]
+    elsif group.first.success
+      successes << [k, group.first.ts]
+      [k, nil]
+    else
+      [k, 2 ** group.take(3).reject(&:success).count]
+    end
+  }
+  .to_h
+max_p = probabilities.values.compact.max
+successes.sort_by(&:last).reverse.map(&:first).each_with_index do |k, n|
+  p = max_p * ((1.0 * n / successes.length) ** 3)
+  raise if probabilities[k]
+  probabilities[k] = p
+end
+
+
 binding.pry if Options.include? :interactive
+
 
 # Engage the user with a randomized translation challenge, offering immediate
 # feedback and vocal pronunciation, while recording his performance.
 
+results_index = results.group_by(&:key)
 correct = 0
 wrong = 0
 
 words.entries.shuffle.each do |key, each|
-  word, ask = [:pt, :en].shuffle
+  if (max_p * rand) < probabilities.fetch([key, :pt], 1.0)
+    word, ask = :en, :pt
+  elsif (max_p * rand) < probabilities.fetch([key, :en], 1.0)
+    word, ask = :pt, :en
+  else
+    next
+  end
+
   puts "Translate to #{ask.upcase}: #{each[word]}"
   answer = gets.strip
 
@@ -82,8 +108,8 @@ words.entries.shuffle.each do |key, each|
     puts "Correct!"
     correct += 1
   else
-    previous_answers = results.fetch(key, [])
-      .select { |r| r.fail? and r.ask == ask }
+    previous_answers = results_index.fetch(key, [])
+      .select { |r| r.success == false and r.ask == ask }
       .map(&:answer)
       .compact
 

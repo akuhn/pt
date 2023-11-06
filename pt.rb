a145f83bf1aaca8fbd203e58897aa10e6d8defea
Adrian Kuhn <akuhn@iam.unibe.ch>
2023-11-06 18:45:07 +0000

    Add option --top10 to print top ten areas of improvement.

--- pt.rb
+++ pt.rb
@@ -4,7 +4,7 @@ require 'options_by_example'
 require './quiz'
 
 
-Options = OptionsByExample.new("Usage: $0 [-i, --interactive]").parse(ARGV)
+Options = OptionsByExample.new("Usage: $0 [-i, --interactive] [--top10]").parse(ARGV)
 
 # Read the vocabulary file to extract word pairs, organizing them with unique
 # references for easy retrieval in the performance database.
@@ -21,6 +21,35 @@ quiz.load_results_from_database
 @probabilities = quiz.assign_adaptive_learning_probabilities
 
 binding.pry if Options.include? :interactive
+
+if Options.include? :top10
+
+  top_failures = quiz.results.values
+    # .select { |answers| answers.first.reference.end_with? ?a }
+    .sort_by { |answers|
+      answers.map.with_index { |each, n| each.failure ? 0.618 ** n : 0 }.sum
+    }
+    .reverse
+    .take(10)
+
+  puts "# Top Ten Areas of Improvement"
+  puts
+
+  top_failures.each do |answers|
+
+    lang = answers.first.lang
+    word = quiz.words[answers.first.reference]
+
+    puts "Translate to #{lang.upcase}: #{word.question(lang)}"
+    puts answers.map(&:answer).compact.map(&:strip).reject(&:empty?)
+    puts "Wrong, the correct answer is:\n#{word.answer(lang)}"
+    puts "0%"
+    puts
+  end
+
+  exit
+end
+
 
 # Engage the user with a randomized translation challenge, offering immediate
 # feedback and vocal pronunciation, while recording his performance.

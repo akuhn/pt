6ae5f1b0e300dee4f0dea4c8d212365e1f16a3ad
Adrian Kuhn <akuhn@iam.unibe.ch>
2023-11-06 16:34:22 +0000

    Fix and simplify the adaptive learning algorithm.

--- quiz.rb
+++ quiz.rb
@@ -86,28 +86,26 @@ class Quiz
 
   def assign_adaptive_learning_probabilities
     partitions = @results.values.group_by do |answers|
-      streak = answers.take_while(&:success).length
-      [:fail, :success][streak] or :streak
+      case answers.take_while(&:success).length
+      when 0
+        case answers.take_while(&:failure).length
+        when 0..1
+          :failure
+        else
+          :failure_streak
+        end
+      when 1
+        :success
+      else
+        :streak
+      end
     end
 
-    streaks_weighted_by_age =  apply_weighting_function(
-      partitions[:streak].sort_by { |answers| answers.first.ts }.reverse
-    )
-
-    successes_weighted_by_recency = apply_weighting_function(
-      partitions[:success].sort_by { |answers| answers.first.ts }
-    )
-
-    failures_weighted_by_count_and_age =  apply_weighting_function(
-      partitions[:fail].sort_by { |answers|
-        [answers.take(3).reject(&:success).count, answers.first.ts]
-      }.reverse
-    )
-
     @probabilities = Hash.new.merge(
-      streaks_weighted_by_age,
-      successes_weighted_by_recency,
-      failures_weighted_by_count_and_age,
+      (boost_ancient_entries partitions[:streak]),
+      (boost_recent_and_ancient_entries partitions[:success]),
+      (boost_recent_and_ancient_entries partitions[:failure]),
+      (boost_ancient_entries partitions[:failure_streak]),
     )
   end
 
@@ -115,13 +113,27 @@ class Quiz
   # in an array, enabling a frequency-based selection that adapts to the user's
   # learning progress.
 
-  def apply_weighting_function(array)
-    probabilities = {}
-    array.each_with_index { |answers, n|
-      probabilities[answers.first.reference_ext] = (1.0 * n / array.length) ** 2
-    }
+  def apply_weighting_function(array, &fun)
+    array
+      .map.with_index { |answers, n|
+        probability = fun.call(n.succ, array.length)
+        [answers.first.reference_ext, probability]
+      }
+      .to_h
+  end
 
-    return probabilities
+  def boost_ancient_entries(array)
+    sorted = array.sort_by { |answers| answers.first.ts }.reverse
+    apply_weighting_function(sorted) { |n, len|
+      (1.0 * n / array.length) ** 2
+    }
+  end
+
+  def boost_recent_and_ancient_entries(array)
+    sorted = array.sort_by { |answers| answers.first.ts }
+    apply_weighting_function(sorted) { |n, len|
+      (2.0 * n / array.length - 1.0) ** 2
+    }
   end
 
   # Execute the quiz session, dynamically selecting questions based on adaptive
@@ -218,6 +230,10 @@ class Result < Struct.new(:reference, :pt, :en, :lang, :success, :answer, :ts)
 
   def reference_ext
     [reference, lang]
+  end
+
+  def failure
+    not success
   end
 end
 
